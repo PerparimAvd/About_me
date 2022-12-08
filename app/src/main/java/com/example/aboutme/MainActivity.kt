3d24df9aa9eaa5541b8930b628e93d3b0405211a
PerparimAvd <90686150+PerparimAvd@users.noreply.github.com>
2022-12-08 09:44:12 +0100

    Utilisation binding

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -20,19 +20,23 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        findViewById<Button>(R.id.done_button).setOnClickListener{
+        //findViewById<Button>(R.id.done_button).setOnClickListener{
+            //addNickName(it)
+        //}
+        binding.doneButton.setOnClickListener{
             addNickName(it)
         }
     }
 
     private fun addNickName(view:View){
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+        binding.apply {
+            nicknameText.text = binding.nicknameEdit.text
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
 
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
 
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
